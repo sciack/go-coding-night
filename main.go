206f0c8bd230da8a1362a4034207a893648a29c3
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-04-22 10:17:43 +0800

    Add solution

--- main.go
+++ main.go
@@ -3,5 +3,71 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world")
+	input := 100
+
+	generateFibonacci(input)
+}
+
+func romanToInt(input string) int {
+	numericMap := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+
+	result := 0
+	i := 0
+
+	for i < len(input) {
+		current := numericMap[string(input[i])]
+
+		if i < len(input)-1 {
+			next := numericMap[string(input[i+1])]
+
+			if next > current {
+				result += next - current
+				i += 2
+				continue
+			}
+		}
+
+		result += current
+		i++
+	}
+
+	return result
+}
+
+func generateFibonacci(input int) {
+	memo := map[int]int{}
+	for i := 0; i <= input; i++ {
+		fib := fibonacci(i, memo)
+
+		fmt.Printf("%v ", fib)
+	}
+}
+
+func fibonacci(input int, memo map[int]int) int {
+	if stored, ok := memo[input]; ok {
+		return stored
+	}
+
+	if input == 0 {
+		memo[input] = 0
+		return 0
+	}
+
+	if input == 1 {
+		memo[input] = 1
+		return 1
+	}
+
+	res := fibonacci(input-1, memo) + fibonacci(input-2, memo)
+	memo[input] = res
+
+	return res
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,22 @@ package main
 
 import "testing"
 
-func TestPlaceholder(t *testing.T) {
-	t.Error("Not implemented")
+func TestRomanToInt(t *testing.T) {
+
+	input := "MCMXCIV"
+	expected := 1994
+	result := romanToInt(input)
+	if result != expected {
+		t.Errorf("Test failed : Expected %v |  Actual %v", expected, result)
+	} else {
+		t.Logf("Pass")
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	memo := map[int]int{}
+	var result = fibonacci(6, memo)
+	if result != 8 {
+		t.Errorf("Expecting 8 got %d", result)
+	}
 }
