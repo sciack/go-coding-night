7126e2f228c47dead904c839409cabe7ab80ef12
Sciachero, Mirko <mirko.sciachero@zuhlke.com>
2021-04-22 09:04:59 +0800

    Roman number solution

--- main.go
+++ main.go
@@ -2,6 +2,41 @@ package main
 
 import "fmt"
 
+func decodeChar(value byte) uint {
+	switch value {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'M':
+		return 1000
+	case 'D':
+		return 500
+	default:
+		return 0
+	}
+}
+
+func arabicNumber(roman string) uint {
+	var result uint = 0
+	var inputLen = len(roman)
+	for i := 0; i < inputLen; i++ {
+		var currentChar = decodeChar(roman[i])
+		if i < inputLen-1 && decodeChar(roman[i+1]) > currentChar {
+			currentChar = -currentChar
+		}
+		result += currentChar
+
+	}
+	return result
+}
+
 func main() {
-	fmt.Print("Hello world")
+	fmt.Printf("Arabic number of MMXXI is %d", arabicNumber("MMXXI"))
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,39 @@ package main
 
 import "testing"
 
-func TestPlaceholder(t *testing.T) {
-	t.Error("Not implemented")
+func TestValues(t *testing.T) {
+	var result = arabicNumber("I")
+	if result != 1 {
+		t.Errorf("Expecting 1 got %d", result)
+	}
+	result = arabicNumber("II")
+	if result != 2 {
+		t.Errorf("Expecting 2 got %d", result)
+	}
+
+	result = arabicNumber("III")
+	if result != 3 {
+		t.Errorf("Expecting 3 got %d", result)
+	}
+	result = arabicNumber("IV")
+	if result != 4 {
+		t.Errorf("Expecting 4 got %d", result)
+	}
+	result = arabicNumber("V")
+	if result != 5 {
+		t.Errorf("Expecting 5 got %d", result)
+	}
+	result = arabicNumber("VI")
+	if result != 6 {
+		t.Errorf("Expecting 6 got %d", result)
+	}
+	result = arabicNumber("XIV")
+	if result != 14 {
+		t.Errorf("Expecting 14 got %d", result)
+	}
+
+	result = arabicNumber("XCIX")
+	if result != 99 {
+		t.Errorf("Expecting 99 got %d", result)
+	}
 }
